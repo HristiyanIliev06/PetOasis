8c661636c5902196023b7f21737716367f912424
HristiyanIliev06 <hristiyan2006@gmail.com>
2025-01-27 18:43:29 +0200

    Creating PetHotel model

---
+++ PetOasis/PetOasis/Models/PetHotel.cs
@@ -0,0 +1,18 @@
+﻿namespace PetOasis.Models
+{
+    public class PetHotel
+    {
+        public string? Street { get; set; }
+
+        public string Street_Number { get; set; }
+
+        public IFormFile Outside_View { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Description { get; set; }
+    }
+}
+

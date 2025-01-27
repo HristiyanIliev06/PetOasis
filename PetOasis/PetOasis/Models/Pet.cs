75796ecd9888b3a4d42a8f4e7cc9105f1e840ac2
Ерхан Неби <erhanemin594@gmail.com>
2025-01-27 18:26:24 +0200

    Направих таблица Pet

---
+++ PetOasis/PetOasis/Models/Pet.cs
@@ -0,0 +1,13 @@
+﻿namespace PetOasis.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Species { get; set; } = null!;
+        public string Breed { get; set; } = null!;
+        public int Age { get; set; }
+        public int Weight { get; set; }
+
+    }
+}

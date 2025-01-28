123a8fb7a91cb40726492c01f28a345b19620e13
Ерхан Неби <erhanemin594@gmail.com>
2025-01-28 20:00:13 +0200

    Ограничения за Pet

--- PetOasis/PetOasis/Models/Pet.cs
+++ PetOasis/PetOasis/Models/Pet.cs
@@ -1,13 +1,25 @@
-﻿namespace PetOasis.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetOasis.Models
 {
     public class Pet
     {
+        [Key]
         public int Id { get; set; }
+        [MaxLength(100)]
+        [Required]
         public string Name { get; set; } = null!;
+        [MaxLength(20)]
+        [Required]
         public string Species { get; set; } = null!;
+        [MaxLength(100)]
+        [Required]
         public string Breed { get; set; } = null!;
-        public int Age { get; set; }
-        public int Weight { get; set; }
+        [Required]
+        public uint Age { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "It's not possible for your pet to weigh less than 0 kg!")]
+        public double Weight { get; set; }
         public IFormFile Image { get; set; } = null!;
 
 

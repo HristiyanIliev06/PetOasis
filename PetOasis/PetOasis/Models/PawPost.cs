998a0f61d08678b6d86cbc0ebd0e932002ecdc5c
Ерхан Неби <erhanemin594@gmail.com>
2025-01-28 20:32:28 +0200

    Таблица PawPost

---
+++ PetOasis/PetOasis/Models/PawPost.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetOasis.Models
+{
+    public class PawPost
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public IFormFile? Image { get; set; }
+        public DateTime? When_uploaded {  get; set; }
+        public string? Additional_mentions {  get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}

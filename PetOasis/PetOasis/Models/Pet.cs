26d64af8a770be55de1d440e38cf3352e96e1b28
HristiyanIliev06 <hristiyan2006@gmail.com>
2025-01-28 20:26:48 +0200

    Finishing touches to the model of Pet

--- PetOasis/PetOasis/Models/Pet.cs
+++ PetOasis/PetOasis/Models/Pet.cs
@@ -2,25 +2,42 @@
 
 namespace PetOasis.Models
 {
+    public enum Species
+    {
+        dog,
+        cat,
+        bird
+    }
     public class Pet
     {
         [Key]
         public int Id { get; set; }
+
         [MaxLength(100)]
         [Required]
+        [Display(Name = "Name: ", Prompt = "My pet's name is...")]
         public string Name { get; set; } = null!;
+
         [MaxLength(20)]
         [Required]
-        public string Species { get; set; } = null!;
+        [Display(Name = "Species: ", Prompt = "My pet's species is...")]
+        public Species Species { get; set; } 
+
         [MaxLength(100)]
         [Required]
-        public string Breed { get; set; } = null!;
+        [Display(Name = "Breed (optional): ", Prompt = "My pet's breed is...")]
+        public string? Breed { get; set; }
+
         [Required]
+        [Display(Name = "Age: ", Prompt = "My pet's age is...")]
         public uint Age { get; set; }
+
         [Required]
         [Range(0, double.MaxValue, ErrorMessage = "It's not possible for your pet to weigh less than 0 kg!")]
+        [Display(Name = "Weight (kg): ", Prompt = "My pet weighs...")]
         public double Weight { get; set; }
-        public IFormFile Image { get; set; } = null!;
+
+        public IFormFile? Image { get; set; }
 
 
     }

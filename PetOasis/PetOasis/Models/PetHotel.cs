5b1944ebc5fb9394960fb39013871bfa2b656672
HristiyanIliev06 <hristiyan2006@gmail.com>
2025-01-28 19:40:58 +0200

    Adding constraints to PetHotel

--- PetOasis/PetOasis/Models/PetHotel.cs
+++ PetOasis/PetOasis/Models/PetHotel.cs
@@ -1,21 +1,44 @@
-﻿namespace PetOasis.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetOasis.Models
 {
     public class PetHotel
     {
+
+        [Key]
+        public int Id { get; set; }
+
+        [ServiceStack.DataAnnotations.Unique]
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; } = null!;
 
+
+        [Required]
+        [MaxLength(30)]
         public string City {  get; set; } = null!;
+
+        [Required]
+        [MaxLength(30)]
         public string Street { get; set; } = null!;
 
-        public string Street_Number { get; set; } = null!;
+        [Required]
+        public uint Street_Number { get; set; }
 
-        public IFormFile Outside_View { get; set; } = null!;
+        [Url]
+        public string? Outside_View { get; set; }
 
+        [MaxLength(15)]
         public string Phone { get; set; } = null!;
 
         public string Email { get; set; } = null!;
 
-        public string Description { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
 

9c97475e7f52ef73205949b0e899671fe961d1c1
Ясмин Тонг <yasmin.tong1707@gmail.com>
2025-02-03 21:06:51 +0200

    Update PetOasisContext.cs

--- PetOasis/PetOasis/Models/PetOasisContext.cs
+++ PetOasis/PetOasis/Models/PetOasisContext.cs
@@ -1,7 +1,24 @@
-﻿namespace PetOasis.Models
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PetOasis.Configuration;
+using System.Reflection;
+
+namespace PetOasis.Models
 {
-    public class PetOasisContext
+    public class PetOasisContext : IdentityDbContext<User>
     {
+        public PetOasisContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+        public DbSet<PawPost> PawPosts { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<PetHotel> PetHotels { get; set; }
 
     }
 }

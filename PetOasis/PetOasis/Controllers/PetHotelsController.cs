e3e78b007634bfcc99ded50443ce0ecc1e89e90f
Ясмин Тонг <yasmin.tong1707@gmail.com>
2025-02-20 20:47:30 +0200

    Authorize controller

--- PetOasis/PetOasis/Controllers/PetHotelsController.cs
+++ PetOasis/PetOasis/Controllers/PetHotelsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using PetOasis.Data.Entities;
 
 namespace PetOasis.Controllers
 {
+    [Authorize]
     public class PetHotelsController : Controller
     {
         private readonly PetOasisContext _context;
